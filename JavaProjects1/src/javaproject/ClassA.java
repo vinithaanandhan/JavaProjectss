4265baf151a0544b5a5c887fa9559f2f2ba77857
vinithaanandhan <160104231+vinithaanandhan@users.noreply.github.com>
2024-02-15 17:46:02 +0530

    Initial commit

---
+++ JavaProjects1/src/javaproject/ClassA.java
@@ -0,0 +1,13 @@
+package javaproject;
+
+public class ClassA {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ 
+		System.out.println("Vinitha");
+		System.out.println("Vinitha");
+		System.out.print("Vinitha");
+	}
+
+}
